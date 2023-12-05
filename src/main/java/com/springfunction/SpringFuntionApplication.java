655d6e19510eb4dbe000bfa240c7a72ea08938ff
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-12-05 16:30:36 +0530

    init

---
+++ src/main/java/com/springfunction/SpringFuntionApplication.java
@@ -0,0 +1,13 @@
+package com.springfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFuntionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFuntionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springfunction/SpringFuntionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFuntionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
