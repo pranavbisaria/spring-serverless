a9699c998b04e52ad8b7a7a935a9d3ec38124367
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-12-05 16:41:50 +0530

    add: supplier and consumer

--- src/main/java/com/springfunction/controller/TaskController.java
+++ src/main/java/com/springfunction/controller/TaskController.java
@@ -1,14 +1,27 @@
 package com.springfunction.controller;
 
+import com.springfunction.model.Task;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Component
 public class TaskController {
     @Bean
     public Function<String, String> reverse() {
         return value -> new StringBuilder(value).reverse().toString();
+    }
+
+    @Bean
+    public Supplier<Task> taskSupplier() {
+        return () -> new Task(1L, "Task 1", "Description 1");
+    }
+
+    @Bean
+    public Consumer<String> printMessage() {
+        return System.out::println;
     }
 }
